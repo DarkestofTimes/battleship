72fb0e25beca92b5af4077631dcb3d4f4fe32085
DarkestofTimes <133776070+DarkestofTimes@users.noreply.github.com>
2023-10-30 18:14:37 +0100

    Add animations for gunFire and missiles

--- dist/main.js
+++ dist/main.js
@@ -1336,6 +1336,41 @@ const game = (0,_game_js__WEBPACK_IMPORTED_MODULE_0__.Game)();
 const renderGame = (0,_render_js__WEBPACK_IMPORTED_MODULE_1__.RenderGame)(game);
 renderGame.initGame();
 
+const battleship = document.querySelectorAll(".battleship");
+const cruiser = document.querySelectorAll(".cruiser");
+const destroyer = document.querySelectorAll(".destroyer");
+const guns = document.querySelectorAll(".BGGun");
+const missiles = document.querySelectorAll(".BGMissile");
+const batteryGuns = document.querySelectorAll(".BGBattery > .BGGun");
+const batteryFireCone = document.querySelectorAll(
+  ".BGBattery > .gunFire > .gunFireCone"
+);
+const batteryFireSmoke = document.querySelectorAll(
+  ".BGBattery > .gunFire > .gunFireSmoke"
+);
+
+document.addEventListener("click", (ev) => {
+  battleship.forEach((ship) => ship.classList.add("moveIn"));
+  cruiser.forEach((ship) => ship.classList.add("moveIn"));
+  destroyer.forEach((ship) => ship.classList.add("moveIn"));
+  setTimeout(() => {
+    guns.forEach((gun) => gun.classList.add("rotateGuns"));
+    missiles.forEach((missile) => missile.classList.add("armMissiles"));
+  }, 2000);
+  batteryGuns.forEach((gun) => gun.classList.add("animateGun"));
+  batteryFireCone.forEach((cone) => cone.classList.add("animateFireCone"));
+  batteryFireSmoke.forEach((Cloud) => Cloud.classList.add("animateFireSmoke"));
+  missiles.forEach((missile) => missile.classList.add("animateMissile"));
+  setTimeout(() => {
+    batteryGuns.forEach((gun) => gun.classList.remove("animateGun"));
+    batteryFireCone.forEach((cone) => cone.classList.remove("animateFireCone"));
+    batteryFireSmoke.forEach((Cloud) =>
+      Cloud.classList.remove("animateFireSmoke")
+    );
+    missiles.forEach((missile) => missile.classList.remove("animateMissile"));
+  }, 2000);
+});
+
 })();
 
 /******/ })()
--- src/index.js
+++ src/index.js
@@ -4,3 +4,38 @@ import { RenderGame } from "./render.js";
 const game = Game();
 const renderGame = RenderGame(game);
 renderGame.initGame();
+
+const battleship = document.querySelectorAll(".battleship");
+const cruiser = document.querySelectorAll(".cruiser");
+const destroyer = document.querySelectorAll(".destroyer");
+const guns = document.querySelectorAll(".BGGun");
+const missiles = document.querySelectorAll(".BGMissile");
+const batteryGuns = document.querySelectorAll(".BGBattery > .BGGun");
+const batteryFireCone = document.querySelectorAll(
+  ".BGBattery > .gunFire > .gunFireCone"
+);
+const batteryFireSmoke = document.querySelectorAll(
+  ".BGBattery > .gunFire > .gunFireSmoke"
+);
+
+document.addEventListener("click", (ev) => {
+  battleship.forEach((ship) => ship.classList.add("moveIn"));
+  cruiser.forEach((ship) => ship.classList.add("moveIn"));
+  destroyer.forEach((ship) => ship.classList.add("moveIn"));
+  setTimeout(() => {
+    guns.forEach((gun) => gun.classList.add("rotateGuns"));
+    missiles.forEach((missile) => missile.classList.add("armMissiles"));
+  }, 2000);
+  batteryGuns.forEach((gun) => gun.classList.add("animateGun"));
+  batteryFireCone.forEach((cone) => cone.classList.add("animateFireCone"));
+  batteryFireSmoke.forEach((Cloud) => Cloud.classList.add("animateFireSmoke"));
+  missiles.forEach((missile) => missile.classList.add("animateMissile"));
+  setTimeout(() => {
+    batteryGuns.forEach((gun) => gun.classList.remove("animateGun"));
+    batteryFireCone.forEach((cone) => cone.classList.remove("animateFireCone"));
+    batteryFireSmoke.forEach((Cloud) =>
+      Cloud.classList.remove("animateFireSmoke")
+    );
+    missiles.forEach((missile) => missile.classList.remove("animateMissile"));
+  }, 2000);
+});
